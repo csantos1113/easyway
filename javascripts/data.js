8bbe899134c7dd419725faf3cd7359772ab9587b
Cesar Mac <rasec_1113@hotmail.com>
2016-07-02 14:17:32 -0500

    better things

---
+++ javascripts/data.js
@@ -0,0 +1,52 @@
+var monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+var endDate = new Date();
+endDate.setFullYear(endDate.getFullYear() + 1);
+
+var intervals = {
+	frisun: {
+		interval: 2,
+		nextWeek: 5,
+		startSaturday: false
+	},
+	frimon: {
+		interval: 3,
+		nextWeek: 4,
+		startSaturday: false
+	},
+	satsun: {
+		interval: 1,
+		nextWeek: 6,
+		startSaturday: true
+	},
+	satmon: {
+		interval: 2,
+		nextWeek: 5,
+		startSaturday: true
+	}
+};
+var cali = {
+	code: 'CLO',
+	name: 'Cali'
+};
+var medellin = {
+	code: 'MDE',
+	name: 'Medellín'
+};
+var bucaramanga = {
+	code: 'BGA',
+	name: 'Bucaramanga'
+};
+var trips = [{
+	from: medellin,
+	to: cali
+}, {
+	from: medellin,
+	to: bucaramanga
+}, {
+	from: cali,
+	to: medellin
+}, {
+	from: cali,
+	to: bucaramanga
+}];
--- javascripts/main.js
+++ javascripts/main.js
@@ -14,21 +14,7 @@
 	};
 	Date.prototype.getMonthName = function() {
 		var month = this.getMonth();
-		var names = [
-			'JAN',
-			'FEB',
-			'MAR',
-			'APR',
-			'MAY',
-			'JUN',
-			'JUL',
-			'AUG',
-			'SEP',
-			'OCT',
-			'NOV',
-			'DEC'
-		];
-		return names[month];
+		return monthNames[month];
 	};
 
 	function getStartDate(saturday) {
@@ -48,7 +34,7 @@
 		return currentDate;
 	}
 
-	function getLanLink(destinationCode, destinationName, endDayPad, endMonthPad, endYear, originCode, originName, startDayPad, startMonthPad, startYear ) {
+	function getLanLink(destinationCode, destinationName, endDayPad, endMonthPad, endYear, originCode, originName, startDayPad, startMonthPad, startYear) {
 		var link = 'http://booking.lan.com/es_co/apps/personas/compra' +
 			'?fecha1_dia=' + startDayPad +
 			'&fecha1_anomes=' + startYear + '-' + startMonthPad +
@@ -63,7 +49,7 @@
 			'&vuelos_destino=' + destinationName +
 			'&to_city1=' + destinationCode +
 			'&flex=1' +
-			'&vuelos_fecha_salida_ddmmaaaa=' + startDayPad + '/' + startMonthPad + '/' + startYear+
+			'&vuelos_fecha_salida_ddmmaaaa=' + startDayPad + '/' + startMonthPad + '/' + startYear +
 			'&vuelos_fecha_regreso_ddmmaaaa=' + endDayPad + '/' + endMonthPad + '/' + endYear +
 			'&cabina=Y' +
 			'&nadults=1' +
@@ -106,63 +92,42 @@
 		var originName = encodeURIComponent(from.name);
 		var destinationCode = to.code;
 		var destinationName = encodeURIComponent(to.name);
-		var startDate = getStartDate(true);
-		var endDate = new Date();
-		endDate.setFullYear(endDate.getFullYear() + 1);
-		var breakpoint = 100;
-		var counter = 0;
+		var selected = intervals[$("#options").val()];
+		if (!selected){
+			alert("It's not yet enabled.");
+			throw ("sorry");
+		}
+		var startDate = getStartDate(selected.startSaturday);
 		var urls = '';
 		while (startDate < endDate) {
 			var startDay = startDate.getDate();
 			var startDayPad = pad('00', startDay, true);
 			var startMonthPad = pad('00', startDate.getMonth() + 1, true);
 			var startYear = startDate.getFullYear();
-			var startLabel = `${startDay}${startDate.getMonthName()}`;
-			startDate.addDays(2);
+			var startLabel = `${startDayPad}${startDate.getMonthName()}`;
+			startDate.addDays(selected.interval);
 			var endDay = startDate.getDate();
 			var endDayPad = pad('00', endDay, true);
 			var endMonthPad = pad('00', startDate.getMonth() + 1, true);
 			var endYear = startDate.getFullYear();
-			var endLabel = `${endDay}${startDate.getMonthName()}`;
+			var endLabel = `${endDayPad}${startDate.getMonthName()}`;
 			var lanLink = getLanLink(destinationCode, destinationName, endDayPad, endMonthPad, endYear, originCode, originName, startDayPad, startMonthPad, startYear);
 			var aviancaLink = getAviancaLink(originCode, destinationCode, startLabel, endLabel);
 			urls += `<tr><td>${startLabel} - ${endLabel}</td><td>${aviancaLink}</td><td>${lanLink}</td></tr>`;
-			startDate.addDays(5);
-			counter++;
-			if (counter > breakpoint) {
-				console.log('out');
-				break;
-			}
+			startDate.addDays(selected.nextWeek);
 		}
-		$('#contenido').html(`${$('#contenido').html()}<section><h2>${from.code} -> ${to.code} -> ${from.code}</h2><table>${urls}</table></section>`);
+		$('#content').html(`${$('#content').html()}<section><h2>${from.code} -> ${to.code} -> ${from.code}</h2><table>${urls}</table></section>`);
 	}
-	var cali = {
-		code: 'CLO',
-		name: 'Cali'
-	};
-	var medellin = {
-		code: 'MDE',
-		name: 'Medellín'
-	};
-	var bucaramanga = {
-		code: 'BGA',
-		name: 'Bucaramanga'
-	};
-	var viajes = [{
-		from: medellin,
-		to: cali
-	}, {
-		from: medellin,
-		to: bucaramanga
-	}, {
-		from: cali,
-		to: medellin
-	}, {
-		from: cali,
-		to: bucaramanga
-	}];
-	$('#contenido').html('');
-	for (var i = 0; i < viajes.length; i++) {
-		showFlies(viajes[i].from, viajes[i].to);
+
+	function generateLinks(){
+		$('#content').html('');
+		for (var i = 0; i < trips.length; i++) {
+			showFlies(trips[i].from, trips[i].to);
+		}
 	}
+	$("#options").change(function(){
+		generateLinks();
+	});
+
+	generateLinks();
 })();
